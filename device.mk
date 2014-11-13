b61498a5740388afef12bd84b55128fa7d4bef1e
Ethan Yonker <dees_troy@teamw.in>
2014-11-13 15:22:19 -0600

    TWRP-ify

--- device.mk
+++ device.mk
@@ -37,7 +37,7 @@ endif
 LOCAL_FSTAB := $(LOCAL_PATH)/fstab.flounder
 
 
-TARGET_RECOVERY_FSTAB = $(LOCAL_FSTAB)
+TARGET_RECOVERY_FSTAB = device/htc/flounder/twrp.fstab
 
 PRODUCT_COPY_FILES := \
     $(LOCAL_KERNEL):kernel \
